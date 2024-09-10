84d399af63fe281841639a4505e0f0ef6903c7ab
Eray Aydın <erayaydinn@protonmail.com>
2024-09-10 12:16:56 +0300

    feat: add suspense to the react root

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
+import Loading from './components/Loading'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <p>Hello, World!</p>
+    <Suspense fallback={<Loading />}>
+      <p>Hello, World!</p>
+    </Suspense>
   </StrictMode>
 )
