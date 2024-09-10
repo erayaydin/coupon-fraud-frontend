1fea344f97fb74d9955f220efabed66fc305d961
Eray Aydın <erayaydinn@protonmail.com>
2024-09-10 12:19:24 +0300

    feat: add app component to react root

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode, Suspense } from 'react'
 import Loading from './components/Loading'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Suspense fallback={<Loading />}>
-      <p>Hello, World!</p>
+      <App />
     </Suspense>
   </StrictMode>
 )
